71460e1308dfbcac5467efd487bd37ec45356232
Michael <michaelchan985@gmail.com>
2015-09-04 04:00:50 -0500

    Added application class

---
+++ DreamLink/app/src/main/java/csc4330/mike/dreamlink/activities/DreamLinkApplication.java
@@ -0,0 +1,12 @@
+package csc4330.mike.dreamlink.activities;
+
+import android.app.Application;
+
+/**
+ * Created by Mike on 9/4/15.
+ */
+public class DreamLinkApplication extends Application {
+
+    
+
+}
