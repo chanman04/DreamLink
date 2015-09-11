abe3a240ecebf50fc84786070b83c4c8e1b3cc79
Michael <michaelchan985@gmail.com>
2015-09-11 17:28:25 -0500

    Added Contact class and contact creation

--- DreamLink/app/src/main/java/csc4330/mike/dreamlink/activities/LoginScreen.java
+++ DreamLink/app/src/main/java/csc4330/mike/dreamlink/activities/LoginScreen.java
@@ -1,19 +1,24 @@
 package csc4330.mike.dreamlink.activities;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.parse.ParseUser;
 
 import org.w3c.dom.Text;
 
+import java.text.ParseException;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import csc4330.mike.dreamlink.R;
+import csc4330.mike.dreamlink.components.Contact;
 
 /**
  * Created by Mike on 9/4/15.
@@ -24,6 +29,12 @@ public class LoginScreen extends Activity {
     @Bind(R.id.password_ET) EditText passwordEditText;
     @Bind(R.id.email_ET) EditText emailEditText;
     @Bind(R.id.submit_button) Button submitButton;
+
+    private String userField;
+    private String passwordField;
+    private String emailField;
+
+    Context context;
 
 
     @Override
@@ -39,6 +50,35 @@ public class LoginScreen extends Activity {
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                try{
+
+                    Contact contact = new Contact();
+
+                    if(userEditText.getText().toString().isEmpty())
+                    {
+                        userEditText.setError("UserName cannot be blank");
+                    }
+                    else if(passwordEditText.getText().toString().isEmpty()){
+
+                        passwordEditText.setError("Password field cannot not be blank");
+                    }
+                    else if(emailEditText.getText().toString().isEmpty()){
+                        emailEditText.setError("Email field cannot be blank");
+
+                    }
+                    else {
+                        contact.setUserName(userEditText.getText().toString());
+                        contact.setUserPassword(passwordEditText.getText().toString());
+                        contact.setUserEmail(emailEditText.getText().toString());
+                    }
+                }
+                catch(ParseException e){
+
+                        Toast.makeText(context, "Please correct your entries before submitting", Toast.LENGTH_SHORT);
+                }
+
+
 
             }
         });
