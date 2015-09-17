24086398d4f459ca567dabfd798607fb9e09952e
Kyle Martinez <kmart76@lsu.edu>
2015-09-16 23:54:53 -0500

    Facebook login update

--- DreamLink/app/src/main/java/csc4330/mike/dreamlink/activities/RecordDream.java
+++ DreamLink/app/src/main/java/csc4330/mike/dreamlink/activities/RecordDream.java
@@ -7,8 +7,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import java.io.IOException;
-
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import csc4330.mike.dreamlink.R;
@@ -44,9 +42,9 @@ public class RecordDream extends ActionBarActivity {
 
                     }
 
-                }catch(IOException e){
+                }catch(Exception e){
 
-
+                    return;
 
                 }
             }
