5c02f19f1b36ae5580061d9eb4d62bf91eefb6d0
Kyle Martinez <kmart76@lsu.edu>
2015-11-30 23:12:47 -0600

    Updated branch

--- DreamLink/app/src/main/java/csc4330/mike/dreamlink/activities/DreamView.java
+++ DreamLink/app/src/main/java/csc4330/mike/dreamlink/activities/DreamView.java
@@ -2,17 +2,21 @@ package csc4330.mike.dreamlink.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
-import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
+import com.parse.ParseUser;
 
 import butterknife.Bind;
 import csc4330.mike.dreamlink.R;
+import csc4330.mike.dreamlink.adapters.CommentAdapter;
 
 public class DreamView extends Activity {
 
@@ -20,6 +24,10 @@ public class DreamView extends Activity {
 
     private TextView dreamTitle;
     private TextView dreamEntry;
+    private TextView dreamCommentSubmit;
+    private Button commentButton;
+    private ParseObject dream;
+    private ListView comments;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,20 +36,31 @@ public class DreamView extends Activity {
 
         dreamTitle = (TextView) findViewById(R.id.dream_title);
         dreamEntry = (TextView) findViewById(R.id.dream_entry);
+        dreamCommentSubmit = (TextView) findViewById(R.id.comment_submit_text);
+        commentButton = (Button) findViewById(R.id.comment_submit_button);
+        comments = (ListView) findViewById(R.id.comments_View);
 
         //Populates the layout with the correct parse dream object passed from dreamFeed
         String parseObjId = getIntent().getStringExtra("parse_obj_id");
         ParseQuery<ParseObject> query = ParseQuery.getQuery("DREAM"); //which query to pull here?
-        query.getInBackground(parseObjId, new GetCallback<ParseObject>() {
+        try {
+            dream = query.get(parseObjId);
+            dreamTitle.setText(dream.getString("DREAM_TITLE"));
+            dreamEntry.setText(dream.getString("DREAM_ENTRY"));
+            comments.setAdapter(new CommentAdapter(this,dream));
+        } catch(ParseException e) {
+            Toast.makeText(DreamView.this, "FAILED TO RETRIEVE DREAM", Toast.LENGTH_LONG).show();
+        }
+
+        //Creates a comment object, connects it to the dream and submits it to parse
+        commentButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void done(ParseObject item, ParseException e) {
-                if (e == null) {
-                    // item was found
-                    dreamTitle.setText(item.getString("DREAM_TITLE"));
-                    dreamEntry.setText(item.getString("DREAM_ENTRY"));
-                } else {
-                    Toast.makeText(DreamView.this, "FAILED TO RETRIEVE DREAM", Toast.LENGTH_LONG).show();
-                }
+            public void onClick(View view) {
+                ParseObject comment = new ParseObject("Comment");
+                comment.put("text", dreamCommentSubmit.getText().toString());
+                comment.put("commenter", ParseUser.getCurrentUser());
+                comment.put("dream", dream);
+                comment.saveInBackground(null);
             }
         });
     }
